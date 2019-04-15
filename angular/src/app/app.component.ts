b50a64cef19c713a85aa1f2a96737aee2ff3d63a
Filip Bech-Larsen <filipbech@filipbech.dk>
2019-04-15 12:22:42 +0200

    first

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, ApplicationRef, ViewEncapsulation } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'angular';
+
+  @Input() title = 'Angular';
+
+  @Output() myEvent = new EventEmitter();
+
+  constructor(private appRef: ApplicationRef) {
+    setTimeout(() => {
+      this.title = 'zup';
+      this.appRef.tick();
+      this.myEvent.emit('yo');
+    }, 1000);
+  }
+
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+    AppComponent
+  ],
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private ij: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const AppComponentCustomElement = createCustomElement(AppComponent, { injector: this.ij });
+    customElements.define('my-app', AppComponentCustomElement);
+  }
+ }
