0282ed962285e2772048d9b51be99f1875ab6d8a
Filip Bech-Larsen <filipbech@filipbech.dk>
2019-04-16 08:06:07 +0200

    vue

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,11 +1,24 @@
+/* eslint-disable */
 <template>
   <div>
     <h1>My Vue Web Component</h1>
     <div>{{ msg }}</div>
+    <slot></slot>
+
   </div>
 </template>
+<style>
+  h1 {
+    color:blue;
+  }
+</style>
 <script>
   export default {
-    props: ['msg'] 
+    props: ['msg'],
+    created() {
+      setTimeout(() => {
+        this.$emit('myEvent', {something:true});
+      }, 5000);
+    }
   }
 </script>
