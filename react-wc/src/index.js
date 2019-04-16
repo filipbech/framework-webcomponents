c707db866b6ed6e3cb818dc8b2e599cb8553c964
Filip Bech-Larsen <filipbech@filipbech.dk>
2019-04-16 13:27:26 +0200

    react

--- react-wc/src/index.js
+++ react-wc/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import App from './App';
-import { register } from 'web-react-components';
+import { register, convert } from 'web-react-components';
 
-register(App, 'my-component', ['name'], {
-    onButtonClick: e => new Event('buttonclick', { bubbles: true })
-})
+console.log(convert(App));
+
+
+// register(App, 'my-component', ['name'], {
+//     onButtonClick: e => new Event('buttonclick', { bubbles: true })
+// })
